82924b5bea11b6c2a7b4271663ce674c12333eb1
hbagratyan <bagratyan92@gmail.com>
2019-08-11 19:19:17 +0300

    11.08.19

--- app/src/main/java/com/harrysapps/boxingtimer/MainActivity.java
+++ app/src/main/java/com/harrysapps/boxingtimer/MainActivity.java
@@ -18,4 +18,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
     }
+
+    public void onClickStart (View view) {
+        Intent intent = new Intent(this, TimerActivity.class);
+        startActivity(intent);
+    }
 }
