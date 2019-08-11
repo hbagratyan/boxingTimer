b0a87db1868dc927b973d9481e00483eafba5293
hbagratyan <bagratyan92@gmail.com>
2019-08-11 18:16:25 +0300

    11.08.19

--- app/src/main/java/com/harrysapps/boxingtimer/MainActivity.java
+++ app/src/main/java/com/harrysapps/boxingtimer/MainActivity.java
@@ -1,7 +1,9 @@
 package com.harrysapps.boxingtimer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickOpenSettings (View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
